9c3616aedfec9e3ece925744be27b7a7a34239fa
kendallgmason <93265601+kendallgmason@users.noreply.github.com>
2022-06-17 10:37:32 +0100

    Fetch component close to finished

--- moviemadness/src/App/App.js
+++ moviemadness/src/App/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Fetch from "../Fetch/"
+import Fetch, { MovieFetch } from "../Fetch/"
 import Movie from "../Movie/"
 import Search from "../Search"
 
 
 function App() {
+  MovieFetch();
   return (
     <div className="App">
     <h1>Welcome to Movie Madness!!</h1>
--- moviemadness/src/Fetch/index.js
+++ moviemadness/src/Fetch/index.js
@@ -1,1 +1,29 @@
-<h1> Fetch </h1>
+import React, {useState, useEffect} from 'react';
+
+export const MovieFetch = (movieTitle) => {
+    const [movie, setMovie]= useState(null);
+   
+
+    useEffect (()=> {
+        async function getData(){
+            const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=e068227f&=?Batman`)
+            const data = await response.json();
+            // setDataMovie(data);
+            if (data.Response === 'False') {
+                console.log(`Movie not found`)
+            }; 
+            setMovie(data.Search);
+            console.log(movie);
+            console.log(data.Poster);
+        };
+        getData();
+    }, [])
+
+    return (
+        <div className="movies">
+            
+        </div>
+    )}
+
+
+export default MovieFetch;
