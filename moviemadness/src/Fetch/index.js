c4b8a56cf034c4d36305d5be71acf18ca524a002
kendallgmason <93265601+kendallgmason@users.noreply.github.com>
2022-06-17 14:44:55 +0100

    asda

--- moviemadness/src/Fetch/index.js
+++ moviemadness/src/Fetch/index.js
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from 'react';
 
 export const MovieFetch = ({movieTitle}) => {
-    // const [image, setImage]= useState("");
-    // const [rating, setRating]= useState("");
     const [movie, setMovie]= useState("");
+    const [searchValue, setSearchValue] = useState('');
 
     useEffect (()=> {
         async function getData(){
             const response = await fetch(`https://www.omdbapi.com/?apikey=3b2a6fcc&t=${movieTitle}`)
             const data = await response.json();
+            
             if (data.Response === 'False') {
                 console.log(`Movie not found`)
             }; 
@@ -21,9 +21,11 @@ export const MovieFetch = ({movieTitle}) => {
 
 
     return (
-        <div className="movies">
+        <div className="image-container d-flex justify-content-start m-3">
             <h1>{movie.Title}</h1>
             <img src={movie.Poster} alt="movie details"/>
+            <div className='overlay d-flex align-items-center justify-content-center'>
+					</div>
             <h3>{movie.Actors}</h3> 
             <h3>Released: {movie.Year}, Runtime: {movie.Runtime}</h3>
             <p>{movie.Plot}</p>
